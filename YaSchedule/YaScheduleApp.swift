8cbfc8cabff9be9c37cc1259dea0f572c8bf97ba
Anton <walltouch@gmail.com>
2025-09-28 17:25:15 +0300

    Initial Commit

---
+++ YaSchedule/YaScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YaScheduleApp.swift
+//  YaSchedule
+//
+//  Created by ANTON ZVERKOV on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct YaScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
